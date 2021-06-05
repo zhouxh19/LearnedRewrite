79abb3b880fe04daea0ac7cf8a085c7ad644dca1
110xph <2696590069@qq.com>
2021-06-05 17:24:10 +0800

    initialize

---
+++ main.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+
+import random
+import math
+import hashlib
+import logging
+import argparse
+
+
+import os
+import sqlparse
+import time
+
+import jpype as jp
+from jpype.types import *
+import jpype.imports
+
+
+from mcts import *
+
+from configs import parse_cmd_args
+from database import Database, RewriteState
+from rewriter import rewrite
+from cost_estimator import previous_cost_estimation
+
+"""
+
+Another game using MCTS. based on a comment from 
+atanas1054 on Jun 27, 2017
+
+I want to have a 2-player game where they take turns. In the beginning there are 114 possible actions and they decrease by 1 every time a player makes a move. The game is played for 10 turns (that's the terminal state). I have my own function for the reward.
+
+Here is a sample game tree:
+
+START- available actions to both players -> [1,2,3,4,5,6....112,113,114]
+Player 1 - takes action 5 -> [5,0,0,0,0,0,0,0,0,0] -remove action 5 from available actions
+Player 2 - takes action 32->[5,0,0,0,0,32,0,0,0,0] - remove action 32 from the available actions
+Player 1- takes action 97 ->[5,97,0,0,0,32,0,0,0,0] - remove action 97 from the available actions
+Player 2 takes action 56 -> [5,97,0,0,0,32,56,0,0,0] - remove action 56 from the available actions
+....
+Final (example) game state after each player makes 5 actions -> [5,97,3,5,1,32,56,87,101,8]
+First 5 entries present the actions taken by Player1, second 5 entries present the actions taken by Player 2
+
+Finally, I apply a reward function to this vector [5,97,3,5,1,32,56,87,101,8]
+
+"""
+
+
+if __name__ == "__main__":
+
+    args = parse_cmd_args()
+
+    db = Database(args)
+
+    base_dir = os.path.abspath(os.curdir)
+    local_lib_dir = os.path.join(base_dir, 'libs')
+
+    query_dir = os.path.join(base_dir, 'queries', 'input_sql')
+    for file in sorted(os.listdir(query_dir)):
+        if not file.endswith('.sql'):
+            continue
+
+        with open(os.path.join(query_dir, file), 'r') as sql_file:
+            sqls = [sql.value for sql in sqlparse.parse(sql_file.read()) if sql.get_type() == 'SELECT']
+            for sql in sqls:
+
+                origin_cost = previous_cost_estimation(sql, db)
+                print("origin cost: "+str(origin_cost))
+
+                rewritten_sql = rewrite(sql, args, db, origin_cost, file) # mcts/topdown
+                rewritten_sql = str(rewritten_sql)
+
+                rewritten_cost = previous_cost_estimation(rewritten_sql, db)
+                print("after-rewrite cost: "+str(rewritten_cost))
---
+++ src/main/java/hr/fer/zemris/calcite/rewriter/JDBCProxy.java
@@ -0,0 +1,15 @@
+package hr.fer.zemris.calcite.rewriter;
+
+import org.apache.calcite.schema.SchemaFactory;
+
+/**
+ * Hello world!
+ *
+ */
+public class JDBCProxy 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( args[0] );
+    }
+}
